4cdfc6add8bb1a9e44c31177585875566de81f02
TB <q0015300153@gmail.com>
2022-01-17 16:41:22 +0800

    feat: SSR 安裝選項

--- ec2-new-ubuntu.sh
+++ ec2-new-ubuntu.sh
@@ -44,9 +44,24 @@ INSTALL_REDIS=true
 # 將 $config['servers']['auth'] 的註解取消並寫上相同密碼
 REDIS_PASS=
 
+# SSR 相關
+# 是否安裝並設定 SSR
+INSTALL_SSR=true
+# rendora 設定檔名稱
+RENDORA_CONFIG=config.yml
+# rendora 監聽 port
+RENDORA_LISTEN_PORT=3001
+# ssr 導向 prot
+SSR_PROT=8081
+# chrmoe 監聽 port
+CHROME_PORT=9222
+
 # SSL 相關
 # 是否在本機安裝免費的 SSL 憑證
 installFreeSLL=false
+
+# 此腳本路徑
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # 更新系統
 apt update -y
@@ -107,8 +122,7 @@ else
         php${PHP_VERSION}-xml \
         php${PHP_VERSION}-zip
 fi
-apt update -y
-apt upgrade -y
+
 # 安裝 composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -304,15 +318,30 @@ server {
     if ($rule_3 = "321"){
         rewrite "^/(\w{2}\-\w{2})?\/?([^?]*)" /index.php?_route_=$2&_language_=$1 last;
     }
-
+EOF'
+if $INSTALL_SSR; then
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/default
+    location / {
+        proxy_pass http://localhost:'${RENDORA_LISTEN_PORT}';
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
+        try_files $uri $uri/ /index.php?$args;
+    }
+EOF'
+else
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/default
     location / {
         try_files $uri $uri/ /index.php?$args;
     }
-  
+
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/run/php/php'${PHP_VERSION}'-fpm.sock;
     }
+EOF'
+fi
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/default
 }
 
 # Virtual Host configuration for example.com
@@ -369,16 +398,30 @@ server {
     if ($rule_3 = "321"){
         rewrite "^/(\w{2}\-\w{2})?\/?([^?]*)" /index.php?_route_=$2&_language_=$1 last;
     }
-
+EOF'
+if $INSTALL_SSR; then
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/'${PROJECT}'.conf
+    location / {
+        proxy_pass http://localhost:'${RENDORA_LISTEN_PORT}';
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $http_x_forwarded_proto;
+        try_files $uri $uri/ /index.php?$args;
+    }
+EOF'
+else
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/'${PROJECT}'.conf
     location / {
         try_files $uri $uri/ /index.php?$args;
     }
-  
+
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/run/php/php'${PHP_VERSION}'-fpm.sock;
     }
-
+EOF'
+fi
+bash -c 'cat <<\EOF >> /etc/nginx/sites-available/'${PROJECT}'.conf
     # Enable SSL
     #ssl_certificate "ssl.crt";
     #ssl_certificate_key "ssl.key";
@@ -395,11 +438,204 @@ server {
     }
 }
 EOF'
+
+if $INSTALL_SSR; then
+bash -c 'cat <<\EOF > /etc/nginx/sites-available/'${PROJECT}'.ssr.conf
+server {
+    listen '${SSR_PROT}';
+    server_name localhost;
+    root "/home/www-data/'${PROJECT}'";
+
+    index index.html index.htm index.php;
+
+    rewrite "^/(\w{2}\-\w{2})?\/?sitemap.xml$" /index.php?route=feed/google_sitemap&_language_=$1 last;
+    rewrite ^/googlebase.xml$ /index.php?route=feed/google_base last;
+    rewrite ^/system/download/(.*) /index.php?route=error/not_found last;
+
+    if (!-f $request_filename){
+        set $rule_3 1$rule_3;
+    }
+
+    if (!-d $request_filename){
+        set $rule_3 2$rule_3;
+    }
+
+    if ($uri !~ ".*\.(ico|gif|jpg|jpeg|png|js|css)"){
+        set $rule_3 3$rule_3;
+    }
+
+    if ($rule_3 = "321"){
+        rewrite "^/(\w{2}\-\w{2})?\/?([^?]*)" /index.php?_route_=$2&_language_=$1 last;
+    }
+
+    location / {
+        try_files $uri $uri/ /index.php?$args;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php'${PHP_VERSION}'-fpm.sock;
+    }
+
+    # Enable SSL
+    #ssl_certificate "ssl.crt";
+    #ssl_certificate_key "ssl.key";
+    #ssl_session_timeout 5m;
+    #ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    #ssl_ciphers ALL:!ADH:!EXPORT56:RC4+RSA:+HIGH:+MEDIUM:+LOW:+SSLv3:+EXP;
+    #ssl_prefer_server_ciphers on;
+
+    charset utf-8;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt { access_log off; log_not_found off; }
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOF'
+fi
 ln -s /etc/nginx/sites-available/${PROJECT}.conf /etc/nginx/sites-enabled/${PROJECT}.conf
 chmod 777 /etc/nginx/sites-enabled/${PROJECT}.conf
 chmod 644 /etc/nginx/sites-available/${PROJECT}.conf
 chmod 644 /etc/nginx/sites-available/default
+if $INSTALL_SSR; then
+    ln -s /etc/nginx/sites-available/${PROJECT}.ssr.conf /etc/nginx/sites-enabled/${PROJECT}.ssr.conf
+    chmod 777 /etc/nginx/sites-enabled/${PROJECT}.ssr.conf
+    chmod 644 /etc/nginx/sites-available/${PROJECT}.ssr.conf
+fi
 systemctl reload nginx
+
+# 安裝 SSR
+if $INSTALL_SSR; then
+    # 安裝 chrome
+    cd $SCRIPT_DIR
+    apt install -y make libappindicator1 fonts-liberation gdebi-core
+    apt install -f
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    gdebi --non-interactive google-chrome*.deb
+    # 下載 go
+    wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz && \
+    tar zxvf go1.17.6.linux-amd64.tar.gz
+    PATH=$PATH:$SCRIPT_DIR/go/bin
+    # 下載 rendora
+    git clone https://github.com/rendora/rendora
+    # 編譯 rendora
+    cd ./rendora && make build && make install
+    cd $SCRIPT_DIR
+    # 移除下載項
+    rm -rf google-chrome*.deb go*.tar.gz ./go ./rendora
+
+    # 寫 rendora 設定檔
+bash -c 'cat <<\EOF > '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+debug: false
+listen:
+    address: 0.0.0.0
+    port: '${RENDORA_LISTEN_PORT}'
+EOF'
+if $INSTALL_REDIS; then
+    if [ "${REDIS_PASS}" != "" ]; then
+bash -c 'cat <<\EOF >> '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+cache:
+    type: redis
+    timeout: 6000
+    redis:
+        address: localhost:6379
+        password: '${REDIS_PASS}'
+        db: 0
+EOF'
+    else
+bash -c 'cat <<\EOF >> '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+cache:
+    type: redis
+    timeout: 6000
+    redis:
+        address: localhost:6379
+        db: 0
+EOF'
+    fi
+else
+bash -c 'cat <<\EOF >> '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+cache:
+    type: local
+    timeout: 6000
+EOF'
+fi
+bash -c 'cat <<\EOF >> '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+target:
+    url: "http://localhost:'${SSR_PROT}'"
+backend:
+    url: "http://localhost:'${SSR_PROT}'"
+headless:
+    waitAfterDOMLoad: 5000
+    timeout: 15
+    internal:
+        url: http://localhost:'${CHROME_PORT}'
+    blockedURLs:
+        - "*.png"
+        - "*.jpg"
+        - "*.jpeg"
+        - "*.webp"
+        - "*.gif"
+        - "*.css"
+        - "*.woff2"
+        - "*.svg"
+        - "*.woff"
+        - "*.ttf"
+        - "*.font"
+        - "https://www.youtube.com/*"
+        - "https://www.google-analytics.com/*"
+        - "https://fonts.googleapis.com/*"
+output:
+    minify: true
+filters:
+    userAgent:
+        defaultPolicy: blacklist
+        exceptions:
+            keywords:
+                - bot
+                - slurp
+                - bing
+                - yandex
+                - crawler
+    paths:
+        defaultPolicy: whitelist
+        exceptions:
+            prefix:
+                - /phpMyAdmin
+                - /phpPgAdmin
+                - /phpRedisAdmin
+            exact:
+                - /api/
+server:
+  enable: false
+EOF'
+
+    # 守護執行程式
+    # 安裝 supervisor
+    apt install -y supervisor
+sudo bash -c 'cat << EOF > /etc/supervisor/conf.d/chrome.conf
+[program:chrome]
+directory=/usr/bin
+command=google-chrome-stable --headless --disable-gpu --remote-debugging-port='${CHROME_PORT}'
+numprocs=1
+autostart=true
+autorestart=true
+user=ubuntu
+EOF'
+
+sudo bash -c 'cat << EOF > /etc/supervisor/conf.d/rendora.'${RENDORA_CONFIG}'.conf
+[program:rendora]
+directory=/usr/bin
+command=rendora --config '${SCRIPT_DIR}'/'${RENDORA_CONFIG}'
+numprocs=1
+autostart=true
+autorestart=true
+user=root
+EOF'
+
+    # 啟動 supervisor 守護
+    supervisorctl update
+fi
 
 # 安裝 SSL 憑證
 if $installFreeSLL; then
